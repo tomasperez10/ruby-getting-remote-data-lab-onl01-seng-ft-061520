1a2df395aee841dd38541328865e18e231ba3cec
Tomas Perez <tomasitopg6@gmail.com>
2020-07-04 01:14:33 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -17,7 +17,10 @@ class GetRequester
   end
   
   def parse_json
-    
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+    program["agency"]  
+  end
   end
   
   
