f07df1ff2a7f81d3b78ad7e14e02ffefd1795480
Tomas Perez <tomasitopg6@gmail.com>
2020-07-04 01:12:34 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -11,7 +11,9 @@ class GetRequester
   URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
   
   def get_response_body
-    
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
   end
   
   
